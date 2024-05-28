9e8401e00297f304e5e055abc52572d2dea1c3d5
chenfuqi <1326143109@qq.com>
2024-05-28 10:03:54 +0800

    fix remove eslint

--- jest.config.ts
+++ jest.config.ts
@@ -15,6 +15,8 @@ const config: Config = {
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
 
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
   coveragePathIgnorePatterns: [
     "/node_modules/", // 忽略node_modules目录
     "__tests__",
